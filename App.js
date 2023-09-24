497051eeb5a2365da9a0587fdbf17d583365140a
edtang0809 <edtang0809@gmail.com>
2023-09-24 22:17:33 +0800

    add DishRow and RestaurantScreen

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
+import RestaurantScreen from './screens/RestaurantScreen';
+import { Provider } from 'react-redux';
 
 
 const Stack = createNativeStackNavigator();
@@ -11,11 +13,12 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <TailwindProvider>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
-      </TailwindProvider>
+        <TailwindProvider>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+          </Stack.Navigator>
+        </TailwindProvider>
     </NavigationContainer>
   );
 };
